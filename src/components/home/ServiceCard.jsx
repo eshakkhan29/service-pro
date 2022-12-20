1195c860fd28ef2aad7a65e540e45d2c98f5ff1e
eshakkhan29 <eshakkhan29@gmail.com>
2022-12-20 17:59:19 +0600

    responsive issue Fix

--- src/components/home/ServiceCard.jsx
+++ src/components/home/ServiceCard.jsx
@@ -6,10 +6,10 @@ const ServiceCard = ({ service }) => {
     const { name, image, link, linkForm } = service;
     return (
         <div className='d-flex flex-column flex-lg-row p-0 serviceCard'>
-            <div className='me-lg-3 half-width'>
-                <img className='cardImage img-fluid h-100' src={image} alt={image} />
+            <div className='half-width'>
+                <img className='cardImage' src={image} alt={image} />
             </div>
-            <div className='p-lg-2 px-4 py-3 half-width serviceCardContent'>
+            <div className='p-lg-2 ps-lg-3 px-4 py-3 half-width serviceCardContent'>
                 <h1 className='fs-4 mt-4 text-white'>{name}</h1>
                 <div>
                     <Link className='mt-3 d-inline-block' to={linkForm}> <button className='quoteButton'>Request a Quote <FaArrowRight className='ms-2' /></button></Link> <br />
